f9eccf272c24cf801dd605d634420902ea6e281f
Mitchell Pavlak <mpavlak2018@pingry.org>
2016-09-29 15:02:15 -0400

    skeleton file

---
+++ MontyHall.java
@@ -0,0 +1,27 @@
+**/
+* Monty Hall Class Problem
+* @author Mitchell Pavlak
+* @author Orndorff AP Comp Sci class
+**/
+
+
+public class MontyHall 
+{
+	public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}s
+}
